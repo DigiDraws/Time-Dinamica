e28e38fcd7024fcba4038c7059ad523bc9665e4a
DigiDraws <128950074+DigiDraws@users.noreply.github.com>
2024-02-11 09:58:44 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 let restant = document.querySelector('#Cronometro');
-function calc() {
 
-let fecha = new Date();
-var days = fecha.getDate();
- var hours = fecha.getHours();
- var mins = fecha.getMinutes();
- var seg = fecha.getSeconds();
- //calcular el tiempo restante para concluir la dinamica de digidraws
- var daysR = 11-days;
- var hoursR = 24-hours;
- //var minsR =  (60*24)-((60*hours)+mins)
- var minsR = 60-mins;
- var segR = 60-seg;
-restant.innerHTML = `<span>Días restantes:</span> ${daysR}<br><span>Horas restantes:</span>${hoursR}<br><span>Minutos restantes:</span> ${minsR}<br><span>Segundos restantes:</span> ${segR}`;
-if (daysR === 0 && hoursR === 0 && minsR === 0 && segR === 0) {
-    restant.innerHTML = `La dinamica a terminado`;
-}
+function calc() {
+    let fechaArObj = new Date(new Date().toLocaleString('es-AR', { timeZone: 'America/Argentina/Buenos_Aires' }));
+
+    var days = fechaArObj.getDate();
+    var hours = fechaArObj.getHours();
+    var mins = fechaArObj.getMinutes();
+    var seg = fechaArObj.getSeconds();
+    console.log(hours);
+
+    // Calcular el tiempo restante para concluir la dinámica de digidraws
+    var daysR = 11 - days;
+    var hoursR = 24 - hours;
+    var minsR = 60 - mins;
+    var segR = 60 - seg;
+
+    restant.innerHTML = `<span>Días restantes:</span> ${daysR}<br><span>Horas restantes:</span>${hoursR}<br><span>Minutos restantes:</span> ${minsR}<br><span>Segundos restantes:</span> ${segR}`;
+
+    if (daysR === 0 && hoursR === 0 && minsR === 0 && segR === 0) {
+        restant.innerHTML = `La dinámica ha concluido`;
+    }
 }
 
 calc();
